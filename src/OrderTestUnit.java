409521092864a7567ed5e012c2a168fcb1a01a38
Nurudin Santoso <lambda.solusi@gmail.com>
2023-11-18 22:47:20 +0700

    first time project

--- src/OrderTestUnit.java
+++ src/OrderTestUnit.java
@@ -1,6 +1,13 @@
 public class OrderTestUnit {
     public static void main(String[] args) {
+        //Master
+        Shop shop = new Shop("PDO Shop");
         //Usecase Register Product
+        Katalog katalog = new Katalog(shop);
+        katalog.registerProduct(new Product("1","Pizza Meat lover",40));
+        katalog.registerProduct(new Product("2","Salad",10));
+        katalog.printAllProduct();
+        katalog.findProductById("1");
         //Usecase Order
     }
 }
